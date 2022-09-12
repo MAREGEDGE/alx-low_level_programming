d5e316f78800c3810aca98cfa491a26d3222377f
MAREGEDGE <edgedominicmarege@gmail.com>
2022-09-12 15:18:48 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#includ <stdio.h>
+
+/**
+*main - Print string using printf
+*
+*Returns
+*/
+
+int main(void)
+{
+Print f ('with proper gramar,but the outcome is a piece of art.\n*);
+return (0);
+
+}
