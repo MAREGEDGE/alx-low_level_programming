177621f3517748ab000ce8a5bcd1b618f0bda1d8
MAREGEDGE <edgedominicmarege@gmail.com>
2022-09-12 14:48:29 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#includ <stdio.h>
+
+/**
+*main - Entry point
+*
+*Return; Always 0(success)
+*/
+init main(void)
+{
+chair mystring [] = "\" Programming is like building a multilingual puzzle\n";
+Puts(mystring);
+
+return (0);
+}
