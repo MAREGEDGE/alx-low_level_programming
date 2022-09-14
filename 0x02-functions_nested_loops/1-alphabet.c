cbb61a5b2e11d203dcc55dc6bdca9b18016d8d54
MAREGEDGE <edgedominicmarege@gmail.com>
2022-09-14 03:27:11 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+*print -alphabet - function to print abc
+*
+*Return: 0
+*/
+void print_alphabet(void)
+{
+	chair c;
+
+	for (c = 'a'; <= 'z'; c++)
+	{
+		_putchar(c)
+	}
+_putchar('\n');
+}
