bd4972253cf761c3b1e32dfff634618f53170a41
MAREGEDGE <edgedminicmarege@gmail.com>
2022-09-22 16:45:00 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -3,11 +3,12 @@
 /**
  *_strcat - concatenates the string pointed to by @src to
  * the end of the string pointed to by @dest
- * @dest: String that will be appended
- * @src: String to be concatenated upon
+ *@dest: String that will be appended
+ *@src: String to be concatenated upon
  *
  * Return: return poiner to @dest
  */
+
 char *_strcat(char *dest, char *src)
 {
 
